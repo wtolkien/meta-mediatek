bd09aea8f32ca7bfe56419f99cdfb3d674c3e227
Wolfgang Tolkien <wtolkien@cypress.bc.ca>
2018-05-15 16:22:35 -0700

    turn off kernel audit messages

--- recipes-bsp/u-boot/files/mt7623-evb/mt7623-evb-emmc.h
+++ recipes-bsp/u-boot/files/mt7623-evb/mt7623-evb-emmc.h
@@ -357,7 +357,7 @@
  *                                       UBoot Command
  **********************************************************************************************/
 /* Shell */
-#define CONFIG_SYS_MAXARGS		            8
+#define CONFIG_SYS_MAXARGS		            16
 #define CONFIG_SYS_PROMPT		            "mt7623-uboot > "
 #define CONFIG_COMMAND_HISTORY
 
